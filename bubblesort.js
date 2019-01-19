baf0f46659b46017834d7e4e10b73a46665ce8cb
Mikaela Sheldt <msheldt@gmail.com>
2019-01-19 08:50:58 -0500

    got bubblesort working

--- bubblesort.js
+++ bubblesort.js
@@ -2,23 +2,26 @@ function bubbleSort(array) {
   if (array.length === 0 || array.length === 1) {
     return array;
   }
-  for (let j = array.length-2; j = 0; j --) {
+  for (let j = array.length-1; j >= 0; j --) {
+    let basket = false;
+    console.log('j is', j, );
     for (let i = 0; i < j; i ++) {
-      let basket = false;
+      console.log('i =', i, 'value is', array[i]);
       if (array[i] > array[i+1]) {
         basket = array[i];
         array[i] = array[i+1];
         array[i+1] = basket;
       }
-      if (basket === false) {
-        return array;
-      }
     }
-    return array;
+    if (basket === false) {
+      console.log("I'm in the basket");
+      return array;
+    }
   }
+  return array;
 }
 
-let test = bubbleSort([]);
+let test = bubbleSort([3, 4, 1, 9, 2, 5]);
 
 console.log(test);
 
